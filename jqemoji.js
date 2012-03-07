a89079f568948aad0027dc29695917df9a44ebcb
けいしゃけ <keishake0925@gmail.com>
2012-03-07 17:03:17 +0900

    add codes

--- jqemoji.js
+++ jqemoji.js
@@ -2,7 +2,7 @@ function jqemoji(str){
 	str = String(str);
 	for(i=0;i<str.length;i++){
 		code = str.charCodeAt(i);
-		if(code==55356||code==55357||code==8419||code==9994||code==9995||code==9996||code==9728||code==9748||code==9729||code==9924){
+		if(code==55356||code==55357||code==8419||code==9994||code==9995||code==9996||code==9728||code==9748||code==9729||code==9924||code==9742){
 			return false;
 		}
 	}
